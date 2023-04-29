a7bc2f05674f0bdde01d9d4c31b9fdf24542529e
Nino Annighoefer <nino@ninoan.com>
2023-04-29 18:16:43 +0100

    Make counter and main window

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
-slint::include_modules!();
-
 slint::slint! {
-    export component HelloWorld {
-        Text {
-            text: "hello world";
-            color: green;
+    import { Button, VerticalBox } from "std-widgets.slint";
+
+    export component CounterWindow inherits Window  {
+        in-out property <int> count: 0;
+        callback increment <=> incr_button.clicked;
+        VerticalBox {
+            incr_button := Button {
+                text: "Increment";
+            }
+            Text { text: root.count; }
         }
     }
 }
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+fn make_counter() -> Result<CounterWindow, slint::PlatformError> {
+    let counter = CounterWindow::new()?;
+    let counter_handle = counter.as_weak();
+    counter.on_increment(move || {
+        let counter = counter_handle.unwrap();
+        counter.set_count(counter.get_count() + 1);
+    });
+    Ok(counter)
+}
 
-    let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+slint::slint! {
+    import { Button, VerticalBox } from "std-widgets.slint";
+
+    export component MainWindow inherits Window  {
+        callback make_counter <=> counter_button.clicked;
+        VerticalBox {
+            counter_button := Button {
+                text: "Open counter";
+            }
+        }
+    }
+}
+
+fn make_main_window() -> Result<MainWindow, slint::PlatformError> {
+    let main_window = MainWindow::new()?;
+    Ok(main_window)
+}
+
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = make_main_window()?;
+    let counter = make_counter()?;
+
+    main_window.on_make_counter(move || {
+        counter.show().unwrap();
     });
 
-    ui.run()
+    main_window.run()
 }
