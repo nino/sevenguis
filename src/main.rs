b20847e068b4c9930dbbdd8629e9509518938d59
Nino Annighoefer <nino@ninoan.com>
2023-04-29 18:33:23 +0100

    Add temperature converter

--- src/main.rs
+++ src/main.rs
@@ -24,13 +24,74 @@ fn make_counter() -> Result<CounterWindow, slint::PlatformError> {
 }
 
 slint::slint! {
+    import { HorizontalBox, LineEdit } from "std-widgets.slint";
+
+    export component TempConv inherits Window {
+        in-out property <float> celsius: 0.0;
+        in-out property <float> fahrenheit: 0.0;
+        callback update_fahrenheit(string);
+        callback update_celsius(string);
+        HorizontalBox {
+            celsius_field := LineEdit {
+                text: root.celsius;
+                edited => {
+                    update_celsius(self.text);
+                    fahrenheit_field.text = root.fahrenheit;
+                }
+            }
+            Text { text: "Celsius = "; }
+            fahrenheit_field := LineEdit {
+                text: root.fahrenheit;
+                edited => {
+                    update_fahrenheit(self.text);
+                    celsius_field.text = root.celsius;
+                }
+            }
+            Text { text: "Fahrenheit"; }
+        }
+    }
+}
+
+fn make_temp_conv() -> Result<TempConv, slint::PlatformError> {
+    let temp_conv = TempConv::new()?;
+
+    {
+        let temp_conv_handle = temp_conv.as_weak();
+        temp_conv.on_update_celsius(move |new_text| {
+            let temp_conv = temp_conv_handle.unwrap();
+            if let Ok(celsius) = new_text.parse() {
+                temp_conv.set_celsius(celsius);
+                temp_conv.set_fahrenheit(celsius * 9.0 / 5.0 + 32.0);
+            }
+        });
+    }
+
+    {
+        let temp_conv_handle = temp_conv.as_weak();
+        temp_conv.on_update_fahrenheit(move |new_text| {
+            let temp_conv = temp_conv_handle.unwrap();
+            if let Ok(fahrenheit) = new_text.parse() {
+                temp_conv.set_fahrenheit(fahrenheit);
+                temp_conv.set_celsius((fahrenheit - 32.0) * 5.0 / 9.0);
+            }
+        });
+    }
+
+    Ok(temp_conv)
+}
+
+slint::slint! {
     import { Button, VerticalBox } from "std-widgets.slint";
 
     export component MainWindow inherits Window  {
         callback make_counter <=> counter_button.clicked;
+        callback make_temp_conv <=> temp_conv_button.clicked;
         VerticalBox {
             counter_button := Button {
                 text: "Open counter";
+            }
+            temp_conv_button := Button {
+                text: "Open temperature converter";
             }
         }
     }
@@ -44,9 +105,14 @@ fn make_main_window() -> Result<MainWindow, slint::PlatformError> {
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = make_main_window()?;
     let counter = make_counter()?;
+    let temp_conv = make_temp_conv()?;
 
     main_window.on_make_counter(move || {
         counter.show().unwrap();
+    });
+
+    main_window.on_make_temp_conv(move || {
+        temp_conv.show().unwrap();
     });
 
     main_window.run()
